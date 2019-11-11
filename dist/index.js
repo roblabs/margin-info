6b61398d2811d1e5c904ac0429a686cc1eb99c82
Rob | Labs <roblabs@users.noreply.github.com>
2019-11-11 12:55:35 -0800

    Refactor to work better with POST on AWS Lambda

--- dist/index.js
+++ dist/index.js
@@ -6,33 +6,54 @@ module.exports.margininfo = (event, context, callback) => {
   var vm = require("vm");  // V8 Virtual Machine
 
   var svg;
+  var method;
+  var args;
 
-  switch (event.method) {
+  if (event.multiValueQueryStringParameters) { // GET requests with query params
+    // values are stored in an array so we need to fetch the first element in the array
+    method = event.multiValueQueryStringParameters.method[0];
+    args = event.multiValueQueryStringParameters.args;
+  } else if (event.body) { // POST requests
+    var body = JSON.parse(event.body);
+    method = body.method;
+    args = body.args;
+  } else { // inovke from the lambda console
+    method = event.method;
+    args = event.args;
+  }
+
+  switch (method) {
   case "scalebar":
       var context = fs.readFileSync("./scalebar.js")
       vm.runInThisContext(context)
-      svg = scalebar(event.args[0],
-              event.args[1],
-              event.args[2],
-              event.args[3]);
+
+      /* global scalebar */
+      svg = scalebar(args[0],
+              args[1],
+              args[2],
+              args[3]);
       break;
   case "slope":
       var context = fs.readFileSync("./slopeguide.js")
       vm.runInThisContext(context)
-      svg = slopeguide(event.args[0],
-              event.args[1]);
+
+      /* global slopeguide */
+      svg = slopeguide(args[0],
+              args[1]);
       break;
   case "grid":
       var context = fs.readFileSync("./griddeclinationdiagram.js")
       vm.runInThisContext(context)
-      svg = griddeclinationdiagram(parseFloat(event.args[0]),
-              parseFloat(event.args[1]),
-              event.args[2],
-              event.args[3],
-              event.args[4]);
+
+      /* global griddeclinationdiagram */
+      svg = griddeclinationdiagram(parseFloat(args[0]),
+              parseFloat(args[1]),
+              args[2],
+              args[3],
+              args[4]);
       break;
   default:
-      console.log('You entered these parameters: ', event.args);
+      console.log('You entered these parameters: ', method, args);
       // console.log(usage);
   }
 
@@ -40,9 +61,8 @@ module.exports.margininfo = (event, context, callback) => {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      "Content-Type": 'image/svg+xml'
     },
-    event: event,
-    method: event.method,
     body: svg
   };
 
